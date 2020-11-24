23e7b9656f939486e668b71068668f2c7d54ef84
BB2Xd <74978198+BB2Xd@users.noreply.github.com>
2020-11-24 22:18:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,13 @@
 <?php
 /*
-BY:- @BenchamXD
-
-CHANNEL:- @IndusBots
+BY:- BenchamXd and edited by me
 */
 error_reporting(0);
 
 set_time_limit(0);
 
 flush();
-$API_KEY = $_ENV['BOT_TOKEN'];
+$API_KEY = $_ENV['BOT_TOKEN']; //Your token
 ##------------------------------##
 define('API_KEY',$API_KEY);
 function bot($method,$datas=[]){
@@ -26,14 +24,14 @@ function bot($method,$datas=[]){
     }
 }
  function sendmessage($chat_id, $text, $model){
-	bot('sendMessage',[
-	'chat_id'=>$chat_id,
-	'text'=>$text,
-	'parse_mode'=>$mode
-	]);
-	}
-	
-//==============BENCHAM======================//
+  bot('sendMessage',[
+  'chat_id'=>$chat_id,
+  'text'=>$text,
+  'parse_mode'=>$mode
+  ]);
+  }
+  
+//====================================//
 $update = json_decode(file_get_contents('php://input'));
 $message = $update->message;
 $message_id = $update->message->id;
@@ -41,20 +39,53 @@ $chat_id = $message->chat->id;
 $name = $from_id = $message->from->first_name;
 $from_id = $message->from->id;
 $text = $message->text;
+$fromid = $update->callback_query->from->id;
+$username = $update->message->from->username;
+$chatid = $update->callback_query->message->chat->id;
+$START_MESSAGE = $_ENV['START_MESSAGE'];
 $HELP_MESSAGE = $_ENV['HELP_MESSAGE'];
 $API_TOKEN = $_ENV['API_TOKEN'];
-$START_MESSAGE = $_ENV['START_MESSAGE'];
-$username = $update->message->from->username;
 if($text == '/start')
 bot('sendmessage', [
                 'chat_id' =>$chat_id,
-                'text' =>"***$START_MESSAGE
-
-Send me a city name to find the weather.***",
+                'text' =>"***$START_MESSAGE***",
 'parse_mode'=>"MarkDown",
 ]);
+if($text == '/joke'){
+$jok = json_decode(file_get_contents('https://sv443.net/jokeapi/v2/joke/Any?type=single'),true);
+$joke = $jok['joke'];
+$catg = $jok['category'];
+bot('sendmessage',[
+'chat_id'=>$from_id,
+'text'=>"`$joke`
 
-//=========================BENCHAMXD=================//
+Catogery:- `$catg`
+
+_Type /joke for more_",
+'parse_mode'=>"MarkDown",
+]);
+}if($text == '/get'){
+
+$data = json_decode(file_get_contents("https://quotes.cwprojects.live/random"),true);
+$text = $data['text'];
+$author = $data['author'];
+$tag = "#" . implode(" #", $data['tags']);
+
+bot('sendmessage', [
+                'chat_id' =>$chat_id,
+                'text' =>"
+***QUOTE:- ***`$text`
+
+***AUTHOR:-*** `$author`
+
+***TAGS:-*** $tag",
+'parse_mode'=>"MarkDown",
+                ]);
+}if($text == '/help')
+bot('sendmessage', [
+                'chat_id' =>$chat_id,
+                'text' =>"$HELP_MENU"
+]);
 if($text !== '/start'){
 $resp = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=$text&appid=$API_TOKEN"),true);
 $weather = $resp['weather'][0]['main'];
