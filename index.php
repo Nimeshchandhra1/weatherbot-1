d5575558808722e7e41a61ad4ac7dc0e2e99574c
BB2Xd <74978198+BB2Xd@users.noreply.github.com>
2020-11-24 22:34:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -114,12 +114,5 @@ Country: `$country`",
 
 ]);
     }
-else {
-bot('sendmessage', [
-                'chat_id' =>$chat_id,
-                'text' =>"`Sorry, i couldn't find that city or the city dosen't exist.`",
-'parse_mode'=>"MarkDown",
-                
-]);
 }
 }
