1693e7c9b835a774711b99c065fe19337d0a0242
Nimesh chandhra <68285734+Nimeshchandhra1@users.noreply.github.com>
2020-11-27 15:07:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -114,7 +114,14 @@ Country: `$country`",
 'parse_mode'=>"MarkDown",
 
 ]);
-    }
+    }else {
+bot('sendmessage', [
+                'chat_id' =>$chat_id,
+                'text' =>"***$IN_MSG***",
+'parse_mode'=>"MarkDown",
+                
+]);
+}
 }if(strpos($text,"/bin") !== false){ 
 $bin = trim(str_replace("/bin","",$text)); 
 $data = json_decode(file_get_contents("https://lookup.binlist.net/$bin"),true);
@@ -228,11 +235,4 @@ Mᴇʀᴄʜᴇɴᴛ:- ***$indusmerchant***
 'parse_mode'=>"MarkDown",
                 ]);
                 }
-}else {
-bot('sendmessage', [
-                'chat_id' =>$chat_id,
-                'text' =>"***$IN_MSG***",
-'parse_mode'=>"MarkDown",
-                
-]);
 }
