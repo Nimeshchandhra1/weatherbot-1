98ef71dafcb157dab1c10031f6980a24d6fad749
Nimesh chandhra <68285734+Nimeshchandhra1@users.noreply.github.com>
2020-12-07 11:49:52 +0530

    Up
    Ch

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <?php
-////////BENCHAMIN LOUIS//////
-//CHANNEL:- T.ME/INDUSBOTS////
+/*
+AUTHOR:- RITHUNAND K [BENCHAMXD]
+
+CHANNEL:- @INDUSBOTS
+
+THIS REPO IS LICENSED UNDER GENERAL PUBLIC LICENSE VERSION 3.
+*/
 error_reporting(0);
 
 set_time_limit(0);
 
 flush();
-$API_KEY = $_ENV['BOT_TOKEN']; 
+$API_KEY = $_ENV['API_KEY'];
 ##------------------------------##
 define('API_KEY',$API_KEY);
 function bot($method,$datas=[]){
@@ -43,12 +48,7 @@ $chat_id = $message->chat->id;
 $name = $from_id = $message->from->first_name;
 $from_id = $message->from->id;
 $text = $message->text;
-$fromid = $update->callback_query->from->id;
 $username = $update->message->from->username;
-$chatid = $update->callback_query->message->chat->id;
-$callback_query = $update->callback_query->data;
-$messageid = $update->callback_query->message->message_id;
-$reply = $update->message->reply_to_message->message_id;
 $START_MESSAGE = $_ENV['START_MESSAGE'];
 //===============BENCHAM=============//
 if ($text == "/start") {
@@ -57,47 +57,28 @@ if ($text == "/start") {
                 'chat_id' =>$chat_id,
                 'text' =>"***$START_MESSAGE
 
-Use*** `/iban xxxxx` ***to check bin on bin-su.***",
+Send your Ekart Tracking ID***",
  'parse_mode'=>'MarkDown',
             
         ]);
- }if(strpos($text,"/iban") !== false){ 
-$text = trim(str_replace("/iban","",$text)); 
-
-$data = json_decode(file_get_contents("https://openiban.com/validate/$text?getBIC=true&validateBankCode=true"),true);
-$value = $data['valid'];
-$indusbots1 = $data['iban'];
-$indusbots2 = $data['bankData']['bankCode'];
-$indusbots3 = $data['bankData']['name'];
-$indusbots4 = $data['bankData']['bic'];
-$indusbots5 = $data['messages'][0];
-
- if($data['valid']){
+ }
+if ($text !== "/start"){
+$indusdata = json_decode(file_get_contents("https://openiban.com/validate/$text?getBIC=true&validateBankCode=true"),true);
+$indus = $indusdata['valid'];
+$ind = $indusdata['iban'];
+$indusu = $indusdata['bankData']['bankCode'];
+$indu = $indusdata['bankData']['name'];
+$indusup = $indusdata['bankData']['bic'];
+$indusupd = $indusdata['messages'][0];
+if($indusdata['iban']){
 bot('sendmessage', [
                 'chat_id' =>$chat_id,
-                'text' =>"***VALID iban✅
-               
-➤ Bɪɴ : $indusbots1
-
-➤ Tʏᴘᴇ : $indusbots2
-
-➤ Bʀᴀɴᴅ : $indusbots3
-
-➤ Bᴀɴᴋ : $indusbots4
-
-➤ Cᴏᴜɴᴛʀʏ : $indusbots5
-
-
-🔺BIN CHECKED FROM DATABASE OF BIN-SU🔻***",
+                'text' =>"Tʀᴀᴄᴋɪɴɢ ɪᴅ :- $ind
+ $indusu
+ $indu
+ $indusup",
 'parse_mode'=>"MarkDown",
-]);
-    }
-else {
-bot('sendmessage', [
-                'chat_id' =>$chat_id,
-                'text' =>"INVALID BIN❌",
-               
-]);
+                ]);
+                }
 }
-}
-
+ 
