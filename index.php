453b2333243dc3650ebc8982393d10d316b27ac5
Nimesh chandhra <68285734+Nimeshchandhra1@users.noreply.github.com>
2020-12-15 19:24:30 +0530

    U

--- index.php
+++ index.php
@@ -154,7 +154,7 @@ Checked By @$username***",
     }
 }if($text == '/gn'){
 $indy = json_decode(file_get_contents("https://randomuser.me/api/1.3"),true);
-$gender = $indy['results']['gender'];
+$gender = $indy['results'][0]['gender'];
 $first = $indy['results'][0]['name']['first'];
 $last = $indy['results'][0]['name']['last'];
 $lasti = $indy['results'][0]['last'];
